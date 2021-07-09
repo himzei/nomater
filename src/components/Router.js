db0ad5f86d1516ded9572298a3945c4ceab40299
조현일 <himzei@gmail.com>
2021-07-10 08:58:13 +0900

    updates

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Auth from "../routes/Auth";
-import Home from "routes/Home";
+import Home from "../routes/Home";
 import Navigation from "./Navigation";
 import Profile from "../routes/Profile";
 import { GlobalStyle } from "../GlobalStyles";
