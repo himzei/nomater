e4d78fb883efe586403d6fed912a8dd5eaeb7df4
조현일 <himzei@gmail.com>
2021-07-10 10:00:52 +0900

    7

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Auth from "routes/Auth";
-import Home from "routes/Home";
+import Home from "../routes/Home";
 import Navigation from "components/Navigation";
 import Profile from "routes/Profile";
 import { GlobalStyle } from "GlobalStyles";
